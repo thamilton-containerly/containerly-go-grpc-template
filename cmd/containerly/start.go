f304547adb0466694ca23145e9cde9771bee1a4c
thamilton <thamilton@containerly.io>
2022-12-29 18:29:33 -0500

    Adding some final touches

--- cmd/containerly/start.go
+++ cmd/containerly/start.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	rpc "{{ $base }}/rpc"
 )
+
+type Server struct {
+	rpc.UnimplementedContainerlyServer
+}
 
 var (
 	startCmd = &cobra.Command{
@@ -10,7 +19,19 @@ var (
 		Short: "start",
 		Long:  `start`,
 		Run: func(cmd *cobra.Command, args []string) {
+			s := grpc.NewServer()
+			lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%v", "8080"))
+			if err != nil {
+				log.Fatalf("failed to listen: %v", err)
+			}
 
+			rpc.RegisterContainerlyServer(s, &Server{})
+
+			log.Printf("server listening at %v", lis.Addr())
+			err = s.Serve(lis)
+			if err != nil {
+				log.Fatalf("failed to serve: %v", err)
+			}
 		},
 	}
 )
