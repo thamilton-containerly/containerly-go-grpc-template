f5cbf358730d1ce1285ae4d55f810109c522b53d
thamilton <thamilton@containerly.io>
2022-12-29 12:01:04 -0500

    Adding start fix

--- cmd/{{ $base }}/start.go
+++ cmd/{{ $base }}/start.go
@@ -2,16 +2,7 @@ package main
 
 import (
 	"github.com/spf13/cobra"
-	"google.golang.org/grpc"
 )
-
-type Service struct {
-	Server *grpc.Server
-}
-
-type Server struct {
-	containerly.UnimplementedContainerlyServer
-}
 
 var (
 	startCmd = &cobra.Command{
