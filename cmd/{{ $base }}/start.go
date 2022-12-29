e3d69bad273a252f109ad8e6d5d8461fd33e6f47
thamilton <thamilton@containerly.io>
2022-12-29 12:00:19 -0500

    Adding proto

--- cmd/{{ $base }}/start.go
+++ cmd/{{ $base }}/start.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	containerly "containerly-go-grpc-template/rpc"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
 )
@@ -20,17 +19,7 @@ var (
 		Short: "start",
 		Long:  `start`,
 		Run: func(cmd *cobra.Command, args []string) {
-			//s := grpc.NewServer()
-			//lis, err := net.Listen("tcp", fmt.Sprintf(":%v", "50051"))
-			//if err != nil {
-			//	log.Fatalf("failed to listen: %v", err)
-			//}
-			//containerly.RegisterContainerlyServer(s, &Server{})
-			//log.Printf("server listening at %v", lis.Addr())
-			//err = s.Serve(lis)
-			//if err != nil {
-			//	log.Fatalf("failed to serve: %v", err)
-			//}
+
 		},
 	}
 )
